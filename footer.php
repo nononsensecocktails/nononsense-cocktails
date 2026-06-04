91dab2cf79daeb5beac9d90c52a230215a18d94b
nononsensecocktails <no.nonsense.cocktails.filter@gmail.com>
2026-06-03 20:44:16 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+    </div> <!-- end main-container -->
+
+    <!-- Footer with simple text links -->
+    <div class="text-center py-3" style="font-size:0.85rem;width:800px;margin:0 auto;">
+        <a href="index.php" class="mx-2">Home</a> |
+        <a href="about.php" class="mx-2">About</a> |
+        <a href="family.php" class="mx-2">Family</a> |
+        <a href="mixers.php" class="mx-2">Mixers</a> |
+        <a href="legal.php" class="mx-2">Legal</a> |
+        <a href="mailto:no.nonsense.cocktails.filter@gmail.com" class="mx-2">Email</a>
+        <div class="mt-2">
+            Drink Responsibly<br>
+            Copyright © No-Nonsense Cocktails <?php echo date('Y'); ?>. All rights reserved.
+        </div>
+    </div>
+
+</body>
+</html>
