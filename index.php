8b81f02e4457fba457e060f73d988b070ece3d2b
nononsensecocktails <no.nonsense.cocktails.filter@gmail.com>
2026-06-12 19:43:15 -0500

    Fixed mobile zoom issue

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ $usernames = getUsernames($conn);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=800, initial-scale=1.0, minimum-scale=0.1, maximum-scale=10.0, user-scalable=yes">
+
     <title>No-Nonsense Cocktails</title>
    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
