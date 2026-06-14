13f50e88d05b9760ba1ac1f1995b692eacde80af
nononsensecocktails <no.nonsense.cocktails.filter@gmail.com>
2026-06-14 13:00:40 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,8 +6,8 @@
         <a href="about.php" class="mx-2">About</a> |
         <a href="family.php" class="mx-2">Family</a> |
         <a href="mixers.php" class="mx-2">Mixers</a> |
-        <a href="legal.php" class="mx-2">Legal</a> |
-        <a href="mailto:no.nonsense.cocktails.filter@gmail.com" class="mx-2">Email</a>
+        <a href="legal.php" class="mx-2">Legal/Privacy</a> |
+        <a href="mailto:no.nonsense.cocktails.filter@gmail.com" class="mx-2">Contact</a>
         <div class="mt-2">
             Drink Responsibly<br>
             Copyright © No-Nonsense Cocktails <?php echo date('Y'); ?>. All rights reserved.
