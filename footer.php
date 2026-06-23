5d255f6c173036183d3b7720b58de4c2389db76e
nononsensecocktails <no.nonsense.cocktails.filter@gmail.com>
2026-06-23 12:50:03 -0500

    Add social media link to footer

--- footer.php
+++ footer.php
@@ -8,6 +8,13 @@
         <a href="mixers.php" class="mx-2">Mixers</a> |
         <a href="legal.php" class="mx-2">Legal/Privacy</a> |
         <a href="mailto:no.nonsense.cocktails.filter@gmail.com" class="mx-2">Contact</a>
+        <div class="mt-2 d-flex justify-content-center">
+            <a href="https://x.com/CocktailsFilter" target="_blank" rel="noopener noreferrer" aria-label="Follow on X" class="mx-2 text-secondary">
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+                </svg>
+            </a>
+        </div>
         <div class="mt-2">
             Drink Responsibly<br>
             Copyright © No-Nonsense Cocktails <?php echo date('Y'); ?>. All rights reserved.
