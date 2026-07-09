67490c5b2fb4c5785363f94d0d71b1cd31e50701
nononsensecocktails <no.nonsense.cocktails.filter@gmail.com>
2026-07-08 20:56:38 -0500

    Update Auth0 configuration to include cookieSecret
    Added cookieSecret to Auth0 SDK configuration.

--- auth/config.php
+++ auth/config.php
@@ -1,15 +1,12 @@
 <?php
 
 // Auth0 Configuration Loader
-// This file loads environment variables and prepares the Auth0 SDK
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use Auth0\SDK\Auth0;
 use Auth0\SDK\Configuration\SdkConfiguration;
 
-// Load .env file
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
@@ -18,8 +15,8 @@ $config = new SdkConfiguration(
     domain: $_ENV['AUTH0_DOMAIN'],
     clientId: $_ENV['AUTH0_CLIENT_ID'],
     clientSecret: $_ENV['AUTH0_CLIENT_SECRET'],
-    audience: 'https://' . $_ENV['AUTH0_DOMAIN'] . '/api/v2/',
-    redirectUri: $_ENV['AUTH0_CALLBACK_URL']
+    redirectUri: $_ENV['AUTH0_CALLBACK_URL'],
+    cookieSecret: $_ENV['AUTH0_COOKIE_SECRET']
 );
 
 $auth0 = new Auth0($config);
