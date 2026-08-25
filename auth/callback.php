4e12e9b36bf11de0ccbbc38312ab81acdce7a0e2
nononsensecocktails <no.nonsense.cocktails.filter@gmail.com>
2026-08-25 15:13:33 -0500

    Require session.php instead of starting session

--- auth/callback.php
+++ auth/callback.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 
-session_start();
+require_once __DIR__ . '/../session.php';
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../db.php';
